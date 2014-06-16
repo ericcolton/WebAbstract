6589afe316fd56a042da7353e91c6d9cf420cd58
Eric Colton <ericcolton@gmail.com>
2014-06-16 00:51:42 -0400

    re-arranged everything
    re-arranged everything

--- Example/WebAbstractDemo/AppDelegate.m
+++ Example/WebAbstractDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TestTableViewController.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    TestTableViewController *tvc = [TestTableViewController new];
+    [self.window setRootViewController:tvc];
     [self.window makeKeyAndVisible];
     return YES;
 }
